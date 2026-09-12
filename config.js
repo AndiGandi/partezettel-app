d2332bbd308a69116a4eeee740fd4dc2171785dd
AndiGandi <andi.gandigoogl@gmail.com>
2026-09-12 21:26:46 +0200

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@
 // passiert über die "Row Level Security"-Regeln in der Datenbank,
 // nicht über Geheimhaltung dieser Werte.
 
-const SUPABASE_URL = "https://dqjuzgqigronucpanszn.supabase.co";
+const SUPABASE_URL = "https://hgliouynbdjsqqkskjsc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhnbGlvdXluYmRqc3Fxa3NranNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5MjI1NzMsImV4cCI6MjEwNDQ5ODU3M30.3BTABRmT71GabtYtqPI-kAjh5ysjTnjxIOPv2lydgnc";
 
 // Namen der Storage-Buckets (müssen exakt mit Supabase übereinstimmen)
